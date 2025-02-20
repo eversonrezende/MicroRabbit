21273ab42683b7eb0d99b705ce89ec5240c5bca0
Everson Rezende <rezende.everson@gmail.com>
2025-02-20 17:39:04 -0300

    implement banking microservices

--- MicroRabbit/MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Program.cs
@@ -1,20 +1,33 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Banking Microservices", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservices V1");
+    });
+//}
 
 app.UseHttpsRedirection();
 
--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,14 +1,38 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace MicroRabbit.Infra.IoC;
 
-public class DependencyContainer
+public static class DependencyContainer
 {
-    public static void RegisterServices(IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<BankingDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("BankingDbConnection"));
+        });
+
         //Domain Bus
         services.AddTransient<IEventBus, RabbitMQBus>();
+
+        //Application Services
+        services.AddTransient<IAccountService, AccountService>();
+
+        //Data
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+        return services;
     }
 }
